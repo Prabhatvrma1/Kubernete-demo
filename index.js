e86b43eaac2fe72831016cefdde89405cbef29d0
Prabhat Verma <prabhatsoni0109@gmail.com>
2026-06-30 18:04:52 +0530

    k8s added

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const express = require('express');
-
+import express from "express";
 const app = express();
-
 const port = 3000;
 
-app.get('/' , (req, res) =>{
-    res.json({
-        message: "hello world from k8s container",
-        service: "hello node",
-        pod: port || "unknown",
-        time: new Date().toISOString(),
-    });
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World from K8s Container",
+    service: "Hello Node",
+    pod: process.env.HOSTNAME || "unknown",
+    time: new Date().toISOString(),
+  });
 });
 
-app.get("/ready" , (req, res) =>{
-    res.status(200).send("ready");
-})
+app.get("/ready", (req, res) => {
+  res.status(200).send("Ready");
+});
 
-app.get("/healthy" , (req, res) =>{
-    res.status(200).send("ok done");
-})
+app.get("/healthy", (req, res) => {
+  res.status(200).send("OK");
+});
 
-app.listen(port , () =>{
-    console.log("server is running on port 3000");
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
