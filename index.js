e2d9c4fbff389f36998ee0e5426240bc00e563f0
Prabhat Verma <prabhatsoni0109@gmail.com>
2026-06-30 16:58:05 +0530

    docker files

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const express = require('express');
 
 const app = express();
+
+const port = 3000;
+
+app.get('/' , (req, res) =>{
+    res.json({
+        message: "hello world from k8s container",
+        service: "hello node",
+        pod: port || "unknown",
+        time: new Date().toISOString(),
+    });
+});
+
+app.get("/ready" , (req, res) =>{
+    res.status(200).send("ready");
+})
+
+app.get("/healthy" , (req, res) =>{
+    res.status(200).send("ok done");
+})
+
+app.listen(port , () =>{
+    console.log("server is running on port 3000");
+})
